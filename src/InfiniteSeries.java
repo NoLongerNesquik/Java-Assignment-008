9ca0eb9ca84b7b749b303a21dd8a1853c3563de8
Taro Oshiro <toshiro470@mycr.redwoods.edu>
2022-10-22 19:11:43 -0700

    Made methods myexp  and check to calculate a near infinite series expansion.

--- src/InfiniteSeries.java
+++ src/InfiniteSeries.java
@@ -2,6 +2,26 @@ public class InfiniteSeries {
 
 
     public static void main(String[] args) {
-
+        for (double i = 0.1; i <= 100; i *= 10) {
+            check(i);
+        }
+        for (double i = -0.1; i >= -100; i *= 10) {
+            check(i);
+        }
+    }
+    public static double myexp(double x, int n) {
+        double sum = 1;
+        double numerator = 1;
+        double denominator = 1;
+        for (int i = 1; i <= n; i++){
+            numerator *= x;
+            denominator *= i;
+            sum += numerator/denominator;
+        }
+        return sum;
+    }
+    public static void check(double x) {
+        int n = 100;
+        System.out.println(x + "\t" + myexp(x, n) + "\t" + Math.exp(x));
     }
 }
